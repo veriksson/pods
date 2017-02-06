3394fde1b1e438d609f7c5eeb9d208c5bc6cc5eb
gdh1c <gdh1c@iamnotacrook.net>
2017-02-06 09:49:43 +0000

    Looking good

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import "net/http"
 import "io/ioutil"
 import "regexp" 
 import "sync"
+import "time"
+import "html/template"
+import "sort"
+import "flag"
+
+var Port = flag.String("port", ":6363", "port to listen to :XXXX")
+
+type byEpisodeName []Episode
+
+func (b byEpisodeName) Len() int { return len(b) }
+func (b byEpisodeName) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
+func (b byEpisodeName) Less(i, j int) bool { return b[i].name < b[j].name }
 
 type Episode struct {
 	name	string
@@ -32,8 +44,8 @@ func (p FFPod) FindPodcastURLs(doc *goquery.Document) []Episode {
 	}
 	if casts, ok := m["GetAcastsByChannel#filipandfredrik#0"]; ok {
 		var wg sync.WaitGroup
-		var mux sync.Mutex
 		var episodes []Episode
+		eps := make(chan Episode)
 		for _, cast := range (casts).([]interface{}) {
 			wg.Add(1)
 			title := cast.(map[string]interface{})["name"].(string)
@@ -41,14 +53,18 @@ func (p FFPod) FindPodcastURLs(doc *goquery.Document) []Episode {
 			docUri := strings.Replace(doc.Url.String(), "www.", "embed.", 1)
 			go func(title, url string) {
 				mp3 := p.parseSpecificPage(url)
-				mux.Lock()
-				episodes = append(episodes, Episode { title, mp3 })
-				mux.Unlock()
+				eps <- Episode { title, mp3 }
 				wg.Done()
 			}(title, docUri + url)
 		}
+		go func() {
+			for ep := range eps {
+				episodes = append(episodes, ep)
+			}
+		}()	
+
 		wg.Wait()
-		fmt.Println("Done waiting!")
+		close(eps)
 		return episodes
 	}
 	return nil
@@ -75,16 +91,17 @@ func (p FFPod) parseSpecificPage(url string) string {
 }
 
 type Pod struct {
-	url	string
-	latest	string
-	parser	PodParser
+	url		string
+	parser		PodParser
+	lastUpdate 	time.Time
+	eps 		[]Episode
 }
 
-func (p Pod) getPodcastArchive() (*goquery.Document, error) {
+func (p *Pod) getPodcastArchive() (*goquery.Document, error) {
 	return goquery.NewDocument(p.url)
 }
 
-func (p Pod) Do() {
+func (p *Pod) Do() {
 	d, err := p.getPodcastArchive()
 	if err != nil {
 		fmt.Println(err.Error())
@@ -92,17 +109,123 @@ func (p Pod) Do() {
 	}
 
 	eps := p.parser.FindPodcastURLs(d)
-	for _, ep := range eps {
-		fmt.Println(ep.url)
+
+	p.lastUpdate = time.Now()
+	sort.Sort(sort.Reverse(byEpisodeName(eps)))
+	p.eps = eps
+}
+
+var m sync.Mutex
+var pods = make(map[string]*Pod)
+
+func update() {
+	m.Lock()
+	fmt.Println("Updating podcasts")
+	for name, pod := range pods {
+		fmt.Printf("* %s... ", name)
+		pod.Do()
+		fmt.Println("Done!")
+	}
+	m.Unlock()
+}
+
+func sched() {	
+	update()
+	c:= time.Tick(1 * time.Hour)
+	for _ = range c {
+		update()
 	}
 }
 
+
+
 func main() {
+	flag.Parse()
 	parser := FFPod("Filip & Fredrik")
-	podcast := Pod {
+	podcast := &Pod {
 		url: "https://www.acast.com/filipandfredrik/",
-		latest: "none",
+		lastUpdate: time.Now(),
 		parser: parser,
 	}
-	podcast.Do()
+	pods["filip & fredrik"] = podcast
+	
+	go sched()
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/forceupdate", func(w http.ResponseWriter, r *http.Request) {
+		writeflush := func (s string) {
+			fmt.Fprint(w, s)
+			if f, ok := w.(http.Flusher); ok {
+				f.Flush()
+			}
+		}
+
+		writeflush("Starting update... ")
+		update()
+		writeflush("Done")
+	})
+	http.ListenAndServe(*Port, nil)
 }
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("index").Parse(indextemplate)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		fmt.Println(err.Error())
+		return 
+	}
+	var data []TemplatePod
+
+	m.Lock()
+	for name, pod := range pods {
+		tpod := TemplatePod { Name: name, LastUpdate: pod.lastUpdate, Episodes: make([]TemplateEpisode, len(pod.eps)) }
+		for i := range pod.eps {
+			tpod.Episodes[i] = TemplateEpisode { Title: pod.eps[i].name, URL: pod.eps[i].url }
+		}
+		data = append(data, tpod)
+	}
+	m.Unlock()
+	err =	t.Execute(w, data)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+type TemplateEpisode struct {
+	Title	string
+	URL	string
+}
+
+type TemplatePod struct {
+	Name		string
+	LastUpdate	time.Time
+	Episodes	[]TemplateEpisode
+}
+
+var indextemplate string = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+			<title>Pods</title>
+			<style type="text/css">
+				* {
+					font-family: Terminal, Consolas, Lucida Console;
+				}
+			</style>
+		</head>
+		<body>
+		{{ range . }}
+			<h3><strong>{{ .Name }}</strong></h3>
+			<i>{{ .LastUpdate }}</i><br />
+			<ul>
+			{{ range .Episodes }}
+				<li><a href="{{ .URL }}" target="_blank">{{ .Title }}</a></li>
+			{{ end }}
+			</ul>
+		{{ end }}
+		<br />
+		</body>
+	</html>`
+
+
+			
